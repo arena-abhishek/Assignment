959834a991f521f60112e1f89773d6419ad9bcd6
arena-abhishek <apdesign0001@gmail.com>
2024-07-31 09:43:27 +0530

    add

---
+++ creative mind/Website Assets/cards/ott/card.js
@@ -0,0 +1,14 @@
+/* $(document).ready(function () {
+  $(".card").hover(function () {
+    $(this).find(".card-hover").animate({ scale: "1" }, 500);
+    $(this).find(".video-play-btn").animate({ opacity: "1" }, 500);
+    $(this).find(".details").animate({ opacity: "1" }, 500);
+  },
+  function(){
+    $(this).find(".card-hover").animate({ scale: "0" }, 500);
+    $(this).find(".video-play-btn").animate({ opacity: "0" }, 500);
+    $(this).find(".details").animate({ opacity: "0" }, 500);
+  }
+);
+});
+ */
